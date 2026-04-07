1661d7b0b966abbfc246e54439691896227f2072
Lu1s <lftalwys@gmail.com>
2026-04-07 13:17:16 +0800

    added login route

--- source/routes/web.php
+++ source/routes/web.php
@@ -12,4 +12,9 @@ Route::get('/register', function () {
     return view('register');
 });
 
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::post('/register', [UserController::class, 'register']);
