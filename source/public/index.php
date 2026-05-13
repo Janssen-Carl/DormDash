477710af6232c4d86041568020d3352b34f1ada4
jh2n-sc <djcat211@gmail.com>
2026-05-13 13:46:49 +0800

    Fix index.php

--- source/public/index.php
+++ source/public/index.php
@@ -17,5 +17,5 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Careful
+
 $app->handleRequest(Request::capture());
