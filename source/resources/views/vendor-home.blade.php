1a336085625698266355d65fd249d7962963487e
Lu1s <lftalwys@gmail.com>
2026-05-14 12:03:08 +0800

    temp vendor home page

---
+++ source/resources/views/vendor-home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.vendor-main')
+
+@section('title', 'VendorHome - DormDash')
+
+@section('content')
+<div class="mx-auto max-w-7xl px-8 py-12">
+    <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tight text-zinc-900">Welcome Back, Vendor!</h1>
+        <p class="mt-2 text-zinc-500 text-sm">Here's a quick overview of your store's performance.</p>
+    </div>
+@endsection
