1dff25a5eac531f0ebac4715cf8b715d945778bc
Lu1s <157115523+Lu1sgt@users.noreply.github.com>
2026-05-18 18:25:42 +0800

    fixed route

--- source/routes/web.php
+++ source/routes/web.php
@@ -69,26 +69,18 @@ Route::middleware(['auth', 'role:customer'])->group(function () {
 
 Route::middleware(['auth', 'role:vendor'])->group(function () {
 
-    Route::get('/vendor/home', fn () => view('vendor-home'))
+    Route::get('/vendor-home', fn () => view('vendor-home'))
         ->name('vendor.home');
 
-    Route::get('/vendor-products', fn () => view('pages/vendor-products'));
+    
 
     Route::get('/vendor-profile', fn () => view('pages/vendor-profile'));
-
     Route::get('/vendor-profile/vendor-profile-edit', fn () => view('pages/vendor-profile-edit'));
-
     Route::get('/vendor-profile/vendor-address-add', fn () => view('pages/vendor-address-add'));
+
+    Route::get('/vendor-products', fn () => view('pages/vendor-products'))->name('vendor.products');
+    Route::get('/vendor-product-edit', fn () => view('pages/vendor-product-edit'))->name('vendor.products.edit');
+    Route::get('/vendor-product-add', fn () => view('pages/vendor-product-add'))->name('vendor.products.add');
+    Route::get('/vendor-product-add-bundle', fn () => view('pages/vendor-product-add-bundle'))->name('vendor.products.bundle');
 });
 
-Route::get('/vendor-profile/vendor-product-edit', function () {
-    return view('pages/vendor-product-edit');
-});
-
-Route::get('/vendor-profile/vendor-product-add', function () {
-    return view('pages/vendor-product-add');
-});
-
-Route::get('/vendor-profile/vendor-product-add-bundle', function () {
-    return view('pages/vendor-product-add-bundle');
-});
