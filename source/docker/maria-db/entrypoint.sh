074d8d2100e9c8591d6376ea2f8d29ded01fc12a
jh2n-sc <djcat211@gmail.com>
2026-05-24 21:22:06 +0800

    Add: env for prod
    # Conflicts:
    #	source/.env.example

--- source/docker/maria-db/entrypoint.sh
+++ source/docker/maria-db/entrypoint.sh
@@ -54,8 +54,8 @@ php artisan view:cache
 
 echo "Fixing Laravel permissions..."
 
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+# chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+# chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
 echo "Starting PHP-FPM..."
 
