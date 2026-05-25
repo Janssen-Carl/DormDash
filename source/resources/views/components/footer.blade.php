1f1cdde1ffd6c4ad920f9751b06d8e5795b9f9bc
Janssen12 <jcmm2023-4700-61428@bicol-u.edu.ph>
2026-05-25 22:58:02 +0800

    Update footer.blade.php

--- source/resources/views/components/footer.blade.php
+++ source/resources/views/components/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="mx-auto max-w-7xl px-4 text-center sm:px-6 lg:px-8">
         <p class="text-base font-semibold text-white">DormDash {{ date('Y') }} &copy; All rights reserved</p>
 
-        <p class="mt-2 text-sm text-white/40">Explore our top-selling items!</p>
+        <p class="mt-2 text-sm text-white/40">Spend wisely, snack wisely!</p>
     </div>
 </footer>
